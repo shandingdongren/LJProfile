ff737b09b30023caf6f16f1bde83f98624a84b00
qianfeng <1510410711@qq.com>
2016-03-04 11:43:44 -0800

    就换个接口的返回给

---
+++ git测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git测试
+//
+//  Created by qianfeng on 16/3/4.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git测试/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  git测试
+//
+//  Created by qianfeng on 16/3/4.
+//  Copyright © 2016年 qianfeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    //high估计会根据安康
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
